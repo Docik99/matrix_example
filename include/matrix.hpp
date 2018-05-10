8f6798bb00f37b8e615674eedba00735380b5d85
Docik99 <32065929+Docik99@users.noreply.github.com>
2018-05-11 00:13:00 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -3,16 +3,16 @@
 class matrix_t {
 private:
 	float ** elements_;
-	unsigned int rows_;
-	unsigned int collumns_;
+	std::size_t rows_;
+	std::size_t collumns_;
 public:
 	matrix_t();
 	matrix_t( matrix_t const & other );
 	matrix_t & operator =( matrix_t const & other );
 	~matrix_t();
 
-	unsigned int rows() const;
-	unsigned int collumns() const;
+	std::size_t rows() const;
+	std::size_t collumns() const;
 
 	matrix_t operator +( matrix_t const & other ) const;
 	matrix_t operator -( matrix_t const & other ) const;
