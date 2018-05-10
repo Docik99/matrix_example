f652e1ba061cd73b4432d1e4b6bcf9431ec39380
Docik99 <32065929+Docik99@users.noreply.github.com>
2018-05-11 00:25:52 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -2,26 +2,26 @@
 
 class matrix_t {
 private:
-	float ** elements_;
-	std::size_t rows_;
-	std::size_t collumns_;
+    float ** elements_;
+    std::size_t rows_;
+    std::size_t collumns_;
 public:
-	matrix_t();
-	matrix_t( matrix_t const & other );
-	matrix_t & operator =( matrix_t const & other );
-	~matrix_t();
-
-	std::size_t rows() const;
-	std::size_t collumns() const;
-
-	matrix_t operator +( matrix_t const & other ) const;
-	matrix_t operator -( matrix_t const & other ) const;
-	matrix_t operator *( matrix_t const & other ) const;
-
-	matrix_t & operator -=( matrix_t const & other );
-	matrix_t & operator +=( matrix_t const & other );
-	matrix_t & operator *=( matrix_t const & other );
-
-	std::istream & read( std::istream & stream );
-	std::ostream & write( std::ostream  & stream ) const;
+    matrix_t();
+    matrix_t( matrix_t const & other );
+    matrix_t & operator =( matrix_t const & other );
+    ~matrix_t();
+    
+    std::size_t rows() const;
+    std::size_t collumns() const;
+    
+    matrix_t operator +( matrix_t const & other ) const;
+    matrix_t operator -( matrix_t const & other ) const;
+    matrix_t operator *( matrix_t const & other ) const;
+    
+    matrix_t & operator -=( matrix_t const & other );
+    matrix_t & operator +=( matrix_t const & other );
+    matrix_t & operator *=( matrix_t const & other );
+    
+    std::istream & read( std::istream & stream );
+    std::ostream & write( std::ostream  & stream ) const;
 };
