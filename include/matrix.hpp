82ddb30ec504a9792983db55cdd24675ebf29267
Docik99 <32065929+Docik99@users.noreply.github.com>
2018-06-06 14:53:24 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -2,18 +2,18 @@
 
 class matrix_t {
 private:
-    float ** elements_;
-    unsigned int rows_;
-    unsigned int collumns_;
+    int ** data;
+    unsigned int stroki;
+    unsigned int stolbi;
 public:
     matrix_t();
-    matrix_t(int rows,int collumns);
-    matrix_t( matrix_t const & other );
+    matrix_t(int in_stroki,int in_stolbi);
+    matrix_t( matrix_t const & object );
     matrix_t & operator =( matrix_t const & other );
     ~matrix_t();
     
-    unsigned int rows() const;
-    unsigned int collumns() const;
+    unsigned int Get_Stroki() const;
+    unsigned int Get_Stolbi() const;
     
     matrix_t operator +( matrix_t const & other ) const;
     matrix_t operator -( matrix_t const & other ) const;
