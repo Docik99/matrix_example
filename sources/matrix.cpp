8e4e34f80a2ce55b38e274a0a89c45fe3f889d61
Docik99 <32065929+Docik99@users.noreply.github.com>
2018-06-06 15:22:33 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -63,7 +63,7 @@ unsigned int matrix_t::collumns() const {
 }
 
 matrix_t matrix_t::operator +( matrix_t const & other ) const {
-    matrix_t result(rows_,collumns_);
+    matrix_t result;
     for (unsigned int i = 0; i < rows_; i++){
         for (unsigned int j = 0; j < collumns_; j++){
             result.elements_[i][j] = elements_[i][j] + other.elements_[i][j];
